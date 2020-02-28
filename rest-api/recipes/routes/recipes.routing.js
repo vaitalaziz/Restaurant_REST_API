7daa7e4a26f86e827bbc8d80f6d1ff2614b1f017
M_A_Aziz <m.a.aziz1900@gmail.com>
2020-02-28 20:35:57 +0100

    update URL link

--- rest-api/recipes/routes/recipes.routing.js
+++ rest-api/recipes/routes/recipes.routing.js
@@ -3,8 +3,8 @@ const RecipeController = require("../controllers/recipe.controller");
 
 exports.routesConfig = (app) => {
     // app.post('/create-recipe', [RecipeController.createRecipe]);
-    app.post('/api/post', [RecipeController.createRecipe]);
-    app.get('/fetch-all-recipes', [RecipeController.fetchAllRecipes]);
+    app.post('/v1/recipes/', [RecipeController.createRecipe]);
+    app.get('/v1/recipes/', [RecipeController.fetchAllRecipes]);
 }
 
 
