eefa3ba6d8d4667ca9ec3e76871c2d2d7ddc0d4f
M_A_Aziz <m.a.aziz1900@gmail.com>
2020-03-05 13:21:41 +0100

    added get for to fetch a specific recipe by id, - added delete and update route

--- rest-api/recipes/routes/recipes.routing.js
+++ rest-api/recipes/routes/recipes.routing.js
@@ -5,6 +5,9 @@ exports.routesConfig = (app) => {
     // app.post('/create-recipe', [RecipeController.createRecipe]);
     app.post('/v1/recipes/', [RecipeController.createRecipe]);
     app.get('/v1/recipes/', [RecipeController.fetchAllRecipes]);
+    app.get('/v1/recipes/:id', [RecipeController.fetchRecipe]);
+    app.delete('/v1/recipes/:id', [RecipeController.deleteRecipe]);
+    app.put('/v1/recipes/:id', [RecipeController.updateRecipe]);
 }
 
 
