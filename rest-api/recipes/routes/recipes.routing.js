0496795972d87bfcc1aecbe5f6e9f032674f052b
M_A_Aziz <m.a.aziz1900@gmail.com>
2020-03-05 18:15:10 +0100

    update routes with comments

--- rest-api/recipes/routes/recipes.routing.js
+++ rest-api/recipes/routes/recipes.routing.js
@@ -2,11 +2,15 @@ const RecipeController = require("../controllers/recipe.controller");
 
 
 exports.routesConfig = (app) => {
-    // app.post('/create-recipe', [RecipeController.createRecipe]);
+    // create new recipe
     app.post('/v1/recipes/', [RecipeController.createRecipe]);
+    // fetch all recipe
     app.get('/v1/recipes/', [RecipeController.fetchAllRecipes]);
+    // fetch a specific recipe by id
     app.get('/v1/recipes/:id', [RecipeController.fetchRecipe]);
+    // delete a specific recipe by id 
     app.delete('/v1/recipes/:id', [RecipeController.deleteRecipe]);
+    // update a specific recipe by id
     app.put('/v1/recipes/:id', [RecipeController.updateRecipe]);
 }
 
